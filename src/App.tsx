c643dfa2b5241e7d788cafb77bab2d809c833df6
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-20 19:35:15 +0530

    Content Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <section className="bg-blue-900 h-20 p-2.5 text-white fixed w-full">
-      <div className="h-full flex">
-        <section className="w-1/2 flex items-center">
-          <h1 className="pl-3 text-3xl">LOGO</h1>
-        </section>
-
-        <section className="w-1/2 flex items-center justify-around text-lg">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Contact Us</a>
-          <a href="#">My Profile</a>
-        </section>
-      </div>
-    </section>
+    <div>
+      <Header />
+    </div>
   );
 }
 
