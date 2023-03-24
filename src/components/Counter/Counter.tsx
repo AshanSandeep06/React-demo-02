f039d8009548c2dd52c1aea99a686b198ec23411
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-24 17:40:24 +0530

    Content Refactored in Counter Component With State

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -17,10 +17,34 @@ export default class Counter extends Component<CounterProps, CounterState> {
     };
   }
 
+  increaseCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decreaseCount = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
   render() {
     return (
       <div className="h-80 border bg-blue-800 text-white px-20 flex flex-col justify-center items-center gap-5 rounded-2xl">
         <h1>{this.state.count}</h1>
+
+        <section className="flex gap-6">
+          <button
+            className="text-4xl border-2 w-12 h-12 rounded-lg"
+            onClick={this.increaseCount}
+          >
+            +
+          </button>
+
+          <button
+            className="text-4xl border-2 w-12 h-12 rounded-lg"
+            onClick={this.decreaseCount}
+          >
+            -
+          </button>
+        </section>
       </div>
     );
   }
