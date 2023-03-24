a6c10ad2e5f3a834008d7e2ffb0750017db82311
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-24 16:01:19 +0530

    Content Updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,20 +3,20 @@ import React, { Component } from "react";
 export default class Header extends Component {
   render(): React.ReactNode {
     return (
-        <section className="bg-blue-900 h-20 p-2.5 text-white fixed w-full">
-      <div className="h-full flex">
-        <section className="w-1/2 flex items-center">
-          <h1 className="pl-3 text-3xl">LOGO</h1>
-        </section>
+      <section className="bg-blue-900 h-20 p-2.5 text-white fixed w-full">
+        <div className="h-full flex">
+          <section className="w-1/2 flex items-center">
+            <h1 className="pl-3 text-3xl">LOGO</h1>
+          </section>
 
-        <section className="w-1/2 flex items-center justify-around text-lg">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Contact Us</a>
-          <a href="#">My Profile</a>
-        </section>
-      </div>
-    </section>
+          <section className="w-1/2 flex items-center justify-around text-lg">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact Us</a>
+            <a href="#">My Profile</a>
+          </section>
+        </div>
+      </section>
     );
   }
 }
