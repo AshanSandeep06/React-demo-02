3b03ff05deb86d5e6556791c958d6036a7636d6d
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-24 17:09:58 +0530

    Created CounterProps and CounterState types in Counter.tsx file

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
 
+type CounterProps = {
+    initialValue: number;
+};
+
+type CounterState = {
+    count: number;
+};
+
 export default class Counter extends Component {
   render() {
     return (
