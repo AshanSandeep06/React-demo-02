6e7366c6b6d917022f2e246e9439cf1aa257d63a
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-24 16:58:09 +0530

    Content Updated in Customer Component

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -9,24 +9,20 @@ export default class Content extends Component {
       <>
         <div className="flex justify-center items-center h-screen">
           <Customer
-            {...
-              {
+            {...{
               id: "C001",
               name: "Bimal",
               address: "Galle",
               salary: 25000.0,
-            }
-          }
+            }}
           />
           <Customer
-            {...
-              {
+            {...{
               id: "C001",
               name: "Ruwan",
               address: "Jaffna",
               salary: 25000.0,
-            }
-          }
+            }}
           />
         </div>
       </>
