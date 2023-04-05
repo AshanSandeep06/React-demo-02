03aa609160041706040725ba70edc35da5960bb0
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-05 15:29:14 +0530

    Content Updated

--- src/components/Light1/Light1.tsx
+++ src/components/Light1/Light1.tsx
@@ -1,12 +1,15 @@
-import { Paper } from '@mui/material'
-import React, { Component } from 'react'
+import { Paper } from "@mui/material";
+import React, { Component } from "react";
+import LightbulbCircleIcon from "@mui/icons-material/LightbulbCircle";
 
 export default class Light1 extends Component {
   render() {
     return (
-      <div className='w-full h-screen justify-center items-center'>
-        <Paper></Paper>
+      <div className="w-full h-screen justify-center items-center bg-slate-900">
+        <Paper elevation={3} className="w-1/3 h-1/2 !bg-slate-900">
+          <LightbulbCircleIcon />
+        </Paper>
       </div>
-    )
+    );
   }
 }
