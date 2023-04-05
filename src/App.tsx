970c6fdbaced6b79c796f314b4c68880ab42bd0e
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-05 15:26:23 +0530

    Added Material UI and Material React dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Content from "./components/Content";
+import Light1 from "./components/Light1/Light1";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Content />
+      {/* <Header />
+      <Content /> */}
+
+      <Light1 />
     </div>
   );
 }
---
+++ src/components/Light1/Light1.tsx
@@ -0,0 +1,12 @@
+import { Paper } from '@mui/material'
+import React, { Component } from 'react'
+
+export default class Light1 extends Component {
+  render() {
+    return (
+      <div className='w-full h-screen justify-center items-center'>
+        <Paper></Paper>
+      </div>
+    )
+  }
+}
